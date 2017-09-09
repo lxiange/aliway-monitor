a20aa261576570e81b3f38c49efe22248fa74e94
xiangge.lx <xiangge.lx@alipay.com>
2017-09-09 15:47:06 +0800

    it works!

---
+++ dist/background.js
@@ -0,0 +1,120 @@
+
+const pushedTids = new Set();
+
+const CONFIG_KEY = 'aliway_monitor_params';
+
+let intervalId = null;
+const AliwayUtils = {
+  checkUpdate: () => {
+    Promise.all(AliwayUtils.configParams.forumIds.map((fid) => {
+      return fetch(`https://www.aliway.com/thread.php?fid=${fid}&orderway=postdate&asc=DESC&page=1`, {
+        method: 'get',
+        credentials: 'include',
+      }
+      ).then(resp => resp.arrayBuffer())
+        .then((ab) => {
+          const enc = new TextDecoder('gbk');
+          return enc.decode(ab);
+        })
+        .then((data) => {
+          const datas = [];
+          const parser = new DOMParser();
+          const dom = parser.parseFromString(data, 'text/html');
+          const subjects = dom.getElementsByClassName('subject');
+
+          Array.prototype.forEach.call(subjects,
+            (item) => {
+              const postUrl = item.href;
+              const postTitle = item.innerText;
+              const tid = postUrl.match(/tid=[0-9]+/)[0].substr(4);
+              AliwayUtils.configParams.subKeys.forEach((key) => {
+                if (postTitle.indexOf(key) !== -1 && !pushedTids.has(tid)) {
+                  datas.push({ postUrl: item.href, postTitle: item.innerText, fid, key });
+                  pushedTids.add(tid);
+                }
+              }
+              );
+            }
+          );
+          return datas;
+        }
+        );
+    })).then(item => item.reduce((x, y) => x.concat(y)))
+      .then((dataToPush) => {
+        if (dataToPush.length !== 0) {
+          AliwayUtils.pushToDingRobot(dataToPush, AliwayUtils.configParams.dingRobotWebHook);
+        }
+      });
+  },
+
+  /*
+  data = [
+          {
+              postTitle:'hahachishi',
+              postUrl:'http://www.aliway.com/dsfaffd....',
+              ...
+          },
+          {
+              ...
+          }
+      ]
+  */
+
+  pushToDingRobot: (data, webHook) => {
+    if (data.length === 0) {
+      return;
+    }
+    console.log(data);
+
+    fetch(webHook, {
+      method: 'post',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify({
+        feedCard: {
+          links: // todo: add pic
+          [{ title: '您有新的订阅消息啦~' }].concat(data.map((item) => {
+            return { title: item.postTitle, messageURL: item.postUrl };
+          })),
+        },
+        msgtype: 'feedCard',
+      }),
+    });
+  },
+
+  start: () => {
+    if (intervalId) {
+      console.error('job is running!');
+    } else {
+      intervalId = setInterval(AliwayUtils.checkUpdate, AliwayUtils.configParams.refreshInterval);
+    }
+  },
+  stop: () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+    } else {
+      console.error('no job is running!');
+    }
+  },
+};
+
+
+chrome.runtime.onMessage.addListener(messageReceived);
+AliwayUtils.configParams = JSON.parse(localStorage[CONFIG_KEY]);
+
+function messageReceived(msg) {
+  AliwayUtils.configParams = JSON.parse(localStorage[CONFIG_KEY]);
+  console.log('get msg', msg);
+  if (msg.action === 'start') {
+    AliwayUtils.start();
+  } else if (msg.action === 'stop') {
+    AliwayUtils.stop();
+  } else {
+    console.error('unknown msg!');
+  }
+}
+
--- src/index.jsx
+++ src/index.jsx
@@ -1,94 +1,149 @@
-import React from 'react'
-import { Col, Row, InputNumber, Input, Checkbox, Button } from 'antd'
-import ReactDOM from 'react-dom'
-import Store from './store.jsx'
-import Utils from './aliway.js'
+import React from 'react';
+import { Col, Row, InputNumber, Input, Checkbox, Button } from 'antd';
+import ReactDOM from 'react-dom';
 
 const CheckboxGroup = Checkbox.Group;
 
 const DING_WEBHOOK_PREFIX = 'https://oapi.dingtalk.com/robot/send?access_token=';
 
+
+const DEFAULT_CONFIG = {
+  subKeys: ['盖楼', '票', '转'],
+  dingRobotWebHook: 'https://oapi.dingtalk.com/robot/send?access_token=d25126c6ed27c12c759b3e7619c13678a30f7968716b590edb02cfc7620c0c2b',
+  forumIds: [20, 17],
+  refreshInterval: 3000,
+  isRunnig: false,
+};
+
+const CONFIG_KEY = 'aliway_monitor_params';
+
 class AliwayMonitor extends React.Component {
   constructor(props) {
     super(props);
-    
+    if (!localStorage[CONFIG_KEY]) {
+      localStorage[CONFIG_KEY] = JSON.stringify(DEFAULT_CONFIG);
+    }
+    const configFromStorage = JSON.parse(localStorage[CONFIG_KEY]);
+    this.configParams = configFromStorage;
+
     this.state = {
-      subKeys: Store.subKeys,
-      dingRobotWebHook: Store.dingRobotWebHook,
-      forumIds: Store.forumIds,
-      refreshInterval: Store.refreshInterval,
-      isRunnig: false,
+      ...this.configParams,
+      hasSession: false,
     };
+    console.log(this.state);
   }
 
-  componentDidMount() {
+  // componentWillMount() {
+  //   // const hasSession=this.xxxxxx();
+  //   // this.setState({hasSession:hasSession});
+  //   if (!localStorage[CONFIG_KEY]) {
+  //     localStorage[CONFIG_KEY] = JSON.stringify(DEFAULT_CONFIG);
+  //   }
+  //   const configFromStorage = JSON.parse(localStorage[CONFIG_KEY]);
+  //   this.configParams = configFromStorage;
+  // }
+
+  updateConfig() {
+    console.log('update config', this.configParams);
+    localStorage[CONFIG_KEY] = JSON.stringify(this.configParams);
   }
 
   refreshIntervalOnChange = (value) => {
-    console.log('refreshInterval', value);
     this.setState({ refreshInterval: value });
-    Store.refreshInterval = value;
+    this.configParams.refreshInterval = value;
+    this.updateConfig();
   }
 
   dingRobotWebHookOnChange = (e) => {
-    let value = e.target.value;
-    console.log('dingRobotWebHook', DING_WEBHOOK_PREFIX + value);
+    const value = e.target.value;
     this.setState({ dingRobotWebHook: DING_WEBHOOK_PREFIX + value });
-    Store.dingRobotWebHook = DING_WEBHOOK_PREFIX + value;
+    this.configParams.dingRobotWebHook = DING_WEBHOOK_PREFIX + value;
+    this.updateConfig();
   }
   forumIdsOnChange = (value) => {
-    console.log(value);
-    this.setState({ subKeys: value });
-    Store.subKeys = value;
+    this.setState({ forumIds: value });
+    this.configParams.forumIds = value;
+    this.updateConfig();
   }
   isRunnigOnChange = () => {
     const isRunnig = this.state.isRunnig;
     if (isRunnig) {
       // kill it
-      Utils.stop();
+      console.log('send msg stop');
+      chrome.runtime.sendMessage({ action: 'stop' });
     } else {
       // start it
-      Utils.start();
+      console.log('send msg start');
+      chrome.runtime.sendMessage({ action: 'start' });
     }
     this.setState({ isRunnig: !isRunnig });
+    this.configParams.isRunnig = !isRunnig;
+    this.updateConfig();
+  }
 
+
+  resetStatus = () => {
+    // todo:
+    chrome.runtime.sendMessage({ action: 'stop' });
+    localStorage[CONFIG_KEY] = JSON.stringify(DEFAULT_CONFIG);
+    // this.setState({ ...DEFAULT_CONFIG });
+    window.close();
   }
 
   render() {
     const forumOptions = [
-      { label: 'Apple', value: 20 },
-      { label: 'Pear', value: 17 },
-      { label: 'Orange', value: 33 },
+      { label: '精彩活动', value: 20 },
+      { label: '捣浆糊', value: 17 },
+      { label: '互帮互助', value: 73 },
     ];
     return (
       <div>
         <Row>
           刷新间隔：
-          <InputNumber min={1000} max={24000} defaultValue={this.state.refreshInterval} onChange={this.refreshIntervalOnChange} />
+          <InputNumber
+            min={1000}
+            max={24000}
+            defaultValue={this.state.refreshInterval}
+            onChange={this.refreshIntervalOnChange}
+          />
         </Row>
         <Row>
           钉钉webhook：
-          <Input addonBefore={DING_WEBHOOK_PREFIX} onChange={this.dingRobotWebHookOnChange} defaultValue={"e75912c04f10588ccc496d85766e4dbf22731b9e0ea6bf22d0e1851544282a43"} />
+          <Input
+            addonBefore={DING_WEBHOOK_PREFIX}
+            onChange={this.dingRobotWebHookOnChange}
+            defaultValue={this.state.dingRobotWebHook.substr(DING_WEBHOOK_PREFIX.length)}
+          />
         </Row>
         <Row>
           订阅板块：
-          <CheckboxGroup options={forumOptions} onChange={this.forumIdsOnChange} defaultValue={[20]} />
+          <CheckboxGroup
+            options={forumOptions}
+            onChange={this.forumIdsOnChange}
+            defaultValue={this.state.forumIds}
+          />
         </Row>
         <Row>
-          <Col>
+          <Col span={8}>
             <Button type="primary" onClick={this.isRunnigOnChange}>
               {this.state.isRunnig ? '停止' : '开始'}
             </Button>
           </Col>
-          <Col>
+          <Col span={8}>
             <Button shape="circle" loading={this.state.isRunnig} disabled />
+          </Col>
+          <Col span={8}>
+            <Button
+              onClick={this.resetStatus}
+            >
+              clear
+            </Button>
           </Col>
         </Row>
 
-      </div>
+      </div >
     );
   }
 }
 
 ReactDOM.render(<AliwayMonitor />, document.getElementById('root'));
-// export default AliwayMonitor;
